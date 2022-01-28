fe7b2539600d3720abe92b3967e944c9f0ac93d7
eldomador <johncenajakub@gmail.com>
2022-01-28 10:22:23 +0100

    styled: modification of layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,20 @@
+import styled from "styled-components";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
 export default function Layout({ children }) {
   return (
-    <div>
+    <Container>
       <Navbar />
       {children}
       <Footer />
-    </div>
+    </Container>
   );
 }
