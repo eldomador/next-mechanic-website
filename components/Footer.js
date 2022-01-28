7f6a02124eebbba9a43c0abf1ae7ee7a3417f517
eldomador <johncenajakub@gmail.com>
2022-01-28 10:22:09 +0100

    styled: modification of footer

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const FooterSection = styled.div`
   background: #000;
   color: #fff;
-  height: 60px;
+  min-height: 60px;
   display: flex;
   justify-content: center;
   align-items: center;
