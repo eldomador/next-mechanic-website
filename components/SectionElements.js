57252992f63626aa19789c001af9a5360728cd22
eldomador <johncenajakub@gmail.com>
2022-01-28 10:20:36 +0100

    styled: adding sectionElements

---
+++ components/SectionElements.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Section = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #fff;
+  min-height: 500px;
+`;
+
+export const Heading = styled.h1`
+  color: #000;
+  font-size: 10rem;
+  font-weight: 900;
+`;
