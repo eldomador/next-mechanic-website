d6503c36fa5e44e8b0781235598a45230b376a0d
eldomador <johncenajakub@gmail.com>
2022-02-09 20:21:33 +0100

    feat: adding a map to the contact page

--- pages/kontakt.js
+++ pages/kontakt.js
@@ -5,7 +5,6 @@ import {
   Section,
   TextContainer,
 } from "../components/SectionElements";
-import Garage from "../components/Icons/Garage";
 import { FiPhone } from "react-icons/fi";
 import { MdOutlinePinDrop, MdOutlineEmail } from "react-icons/md";
 
@@ -30,7 +29,13 @@ export default function Contact() {
           </Paragraph>
         </TextContainer>
         <ImageContainer>
-          <Garage />
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d9282.793908453268!2d16.7347959!3d54.4329567!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x5a979c21c648ca9a!2sAuto%20Serwis!5e0!3m2!1spl!2spl!4v1644433817248!5m2!1spl!2spl"
+            width="600"
+            height="450"
+            style={{ border: "0" }}
+            loading="lazy"
+          ></iframe>
         </ImageContainer>
       </Section>
     </>
