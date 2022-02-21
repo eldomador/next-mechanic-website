6b932eda4a52b0166a4b3482edc4dbc26cb59358
eldomador <johncenajakub@gmail.com>
2022-02-21 19:10:06 +0100

    refactor: adding link to button

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import {
   TextContainer,
 } from "../components/SectionElements";
 import { Button } from "../styles/globalStyles";
+import Link from "next/link";
 
 Home.title = "Mechanik Krystian Paź - Warsztat Auto Serwis Staniewice";
 Home.description = "Najlepszy serwis samochodowy w okolicy Sławna";
@@ -15,7 +16,9 @@ export default function Home() {
       <Section>
         <TextContainer>
           <Heading>Najlepszy serwis samochodowy w twojej okolicy</Heading>
-          <Button>Kontakt</Button>
+          <Link href="/kontakt" passHref>
+            <Button>Kontakt</Button>
+          </Link>
         </TextContainer>
         <ImageContainer>
           <Mechanic />
